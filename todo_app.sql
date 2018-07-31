e9526bbcedc1ff4df7a31f675acadd692098446b
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-30 15:18:41 -1000

    create table

--- todo_app.sql
+++ todo_app.sql
@@ -14,3 +14,12 @@ CREATE DATABASE todo_app;
 --connect to todo_app
 \c todo_app;
 
+--Create table named tasks
+CREATE TABLE tasks (
+  id              integer                       NOT NULL,
+  title           varchar(255)                  NOT NULL,
+  description     text,
+  created_at      timestamp without time zone   NOT NULL DEFAULT now(),
+  updated_at      timestamp without time zone,
+  completed       boolean                       NOT NULL DEFAULT false
+);
