9349467347be8943c9a9b2eeaf38b0aa6bc8ffd0
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-30 15:01:16 -1000

    create user michael

--- todo_app.sql
+++ todo_app.sql
@@ -1,4 +1,16 @@
+--Drops database if it exists
 DROP DATABASE IF EXISTS todo_app;
+
+--Drops user if it exists
+DROP USER IF EXISTS michael;
+
+--Create a user = michael
+CREATE USER michael
+WITH ENCRYPTED PASSWORD 'stonebreaker';
+
+--Creates database
 CREATE DATABASE todo_app;
 
-CREATE ROLE michael
+--connect to todo_app
+\c todo_app;
+
